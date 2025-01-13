d2b747862c272cc8c6ef6ec96dd820880d8b6b80
Elena Sukhorukova <suhorukova.el@gmail.com>
2025-01-13 11:19:37 +0400

    Update react

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -22,8 +22,6 @@ const useMarvelService = () => {
 	const getAllComics = async (offset = _baseOffset) => {
 		const res = await request(`${_apiBase}comics?limit=8&offset=${offset}&${_apiKey}`);
 
-		console.log(res);
-
 		return res.data.results.map(_transformComics);
 	}
 
